af9e6c6fa8ecd325116ec6de40cd1dd21210fcfa
sikiryl <saritrat.206@gmail.com>
2024-05-19 20:19:36 +0700

    doc: add document for create command

--- cmd/create.go
+++ cmd/create.go
@@ -15,13 +15,15 @@ const FIRST_ARGUMENT = 0
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Args:  cobra.MaximumNArgs(1),
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "converts the Levis configuration (levis.yaml) file into Kubernetes manifests",
+	Long: `
+Converts the specified YAML file into Kubernetes manifests, simplifying application deployment.
 
-Create is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+For example:
+
+	levis create -f levis.yaml
+
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("create called: ", args[FIRST_ARGUMENT])
 	},
@@ -29,7 +31,7 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(createCmd)
-	createCmd.Flags().BoolP("file", "f", false, "get levis config")
+	createCmd.Flags().BoolP("file", "f", false, "levis config file")
 	err := createCmd.MarkFlagRequired("file")
 	if err != nil {
 		fmt.Println("createCmd MarkFlagRequired Error:", err)
